e1cbd5acdd216616cfec13baf7e61086852f3796
Joey 😎 <43940776+joeyboli@users.noreply.github.com>
2025-08-23 03:52:41 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 DB_PATH="/data/db.sqlite3"
-DUMP_URL="https://db-dumps.lrclib.net/lrclib-db-dump-20250812T075019Z.sqlite3.gz"
+DUMP_URL="https://db-dumps.lrclib.net/lrclib-db-dump-20250823T005259Z.sqlite3.gz"
 
 echo "[lrclib] Checking DB file: $DB_PATH"
 ls -l /data || echo "[lrclib] /data directory does not exist!"
